c4d7be04f3b0f6a0dc136951f6669ab36f18fa92
Ruchika <60167099+ruchi2ch@users.noreply.github.com>
2021-12-23 15:27:00 +0530

    Update Agent.jsx

--- src/Components/Agent.jsx
+++ src/Components/Agent.jsx
@@ -28,7 +28,7 @@ function Map(){
         //      </div>
         //  </div>
           <div style={{width:'100vw',height:'100vh'}}>
-    <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyB0ZljktG34E7AfpCV3fss6gYRw1Gp9MH0`}
+    <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=API`}
     loadingElement={<div style={{height:"100%"}}/>}
     containerElement={<div style={{height:"100%"}}/>}
     mapElement={<div style={{height:"100%"}}/>}
